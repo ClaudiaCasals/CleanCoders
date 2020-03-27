e696a3a03993cdee7250c668c0f5ea1c8348f911
Elena <60514098+Elena-GHub@users.noreply.github.com>
2020-03-27 07:07:12 +0100

    Original simple coders array list, typos fixed
    This is the same CodersList in a simple array. Only some typos have been fixed. Uploaded here for the convenience of having it in the same branch. If the file from two weeks ago is used, it is not a problem.

---
+++ CodersList.php
@@ -0,0 +1,28 @@
+<?php
+namespace App;
+
+$coders = array('Alejandro Acho',
+    'Joana Börsch Peiró',
+    'Sebastián Herrera',
+    'Alen Robira',
+    'Johanna Sánchez Vallejo',
+    'Eva Albors',
+    'Bruna Gomez de Melo',
+    'Axel Cayuela',
+    'Umit Canbatur',
+    'Francisco Correira',
+    'Alejandro Delgado',
+    'Monuir Elimalahi',
+    'Sergi Oliveres',
+    'Kevin Hidalgo Vivar',
+    'Inna Malyutova',
+    'Alberto Guerrero Martínez',
+    'Jose Antonio Ortega Vailén',
+    'Andrea Aguilar Picón',
+    'Sofía Rocío Carballo Callegari',
+    'Diego Córdoba',
+    'Javier Mármol',
+    'Magali Barriga',
+    'Elena Carballido Marín',
+    'Claudia Casals Nerváez');
+return $coders;
